e1b32f90782438099319729c6f8283544c87fcda
RONALDO RODRIGUES <ronaldorodrigues@mobibrasil.com>
2022-06-15 18:00:30 -0300

    tela inicial sem imagens

---
+++ web/src/components/Carrossel.tsx
@@ -0,0 +1,8 @@
+import { Carousel } from "rsuite";
+
+export function Carrossel() {
+    return (
+        // <Carousel className="custom-slider">
+        // {/* </Carousel> */}
+    )
+}
--- web/src/components/pages/Inicio.tsx
+++ web/src/components/pages/Inicio.tsx
@@ -1,20 +1,20 @@
 import { Footer } from "../Footer";
 import { Login } from "../Login";
-import { Carousel } from 'rsuite';
+import { Carrossel } from "../Carrossel";
+import { BookOpen, ChatCircleDots, Circle, CircleNotch, CirclesFour, DotsNine, FileCloud } from "phosphor-react";
 
 
 export function Inicio() {
     return (
         <>
-            <div className="flex-1 grid h-screen w-screen bg-gradient-to-r from-slate-100 to-emerald-100 items-center">
-                {/* <Carousel className="custom-slider">
-                    <img src="https://leiturinha.com.br/blog/wp-content/uploads/2019/01/biblioteca.jpg" height="150"/>
-                    <img src="https://static.appgeek.com.br/imagens/ler-livros-og.jpg" height="150" />
-                    <img src="https://static.vecteezy.com/ti/vetor-gratis/p1/495030-pessoas-lendo-na-grande-pilha-de-livro-com-ilustracao-em-plana-fundo-arvore-vetor.jpg" height="150"/>
-                </Carousel> */}
+            <div className="flex-1 grid h-screen w-screen bg-gradient-to-r from-amber-200 to-orange-400 items-center">
+                
+                <div className="m-40">
 
-
-                <div className="absolute self-center justify-self-end right-16">
+                <span className="font-bold text-[5rem] text-transparent bg-clip-text bg-gradient-to-l from-blue-800 to-blue-700 text-clip flex max-w-4xl">Aproximando bibliotecas</span>
+                <span className="flex items-center gap-2 text-xl">Empréstimos <Circle weight="fill"/> Consultas <Circle weight="fill"/> Renovação </span>
+                </div>
+                <div className="absolute self-center justify-self-end right-40">
                     <Login/>
                 </div>
 
